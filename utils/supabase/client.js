b56d1c30dd2fd5c709b16aa1a22af622905e7f0f
Muhammad-Jay <jsync4172004@gmail.com>
2025-05-29 21:23:30 +0100

    implement markdown and content ui

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -3,7 +3,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
     createBrowserClient(
-        "https://malnozncuicmnfnowtxn.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbG5vem5jdWljbW5mbm93dHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjQyMjIsImV4cCI6MjA2MjY0MDIyMn0.Lid-L1xgBqnJM5q9hIYU95qFUJIME4etnPdEXxAYSRA",
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
