8c1e21ae71d93d105c1f9653cc4348b126ac7f02
Muhammad-Jay <jsync4172004@gmail.com>
2025-11-09 12:55:47 +0100

    feat: Pass contentType and contentId to GenerateContent component

--- app/(root)/dashboard/generate/[type]/[content_id]/page.tsx
+++ app/(root)/dashboard/generate/[type]/[content_id]/page.tsx
@@ -1,7 +1,7 @@
 import {cn} from "@/lib/utils";
 import { GenerateContent } from '@/components/Layout/Dashboard/Generate/GenerateContent';
 
-export default function Generate() {
+export default function Generate({ params }: { params: { type: string, content_id: string } }) {
 
-    return <GenerateContent/>
+    return <GenerateContent contentType={params.type} contentId={params.content_id} />
 }
