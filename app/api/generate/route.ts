9cd35f73a01db718a1d71a1f20c69c34793d3629
Muhammad-Jay <jsync4172004@gmail.com>
2025-11-09 12:42:46 +0100

    feat: Integrate content saving into generate API route

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -2,18 +2,24 @@ import { NextResponse } from "next/server";
 import { streamText } from 'ai';
 import { GENERATOR_PROMPT, PROFESSIONAL_CONTENT_CREATOR } from '@/lib/AI/ai.system.prompt';
 import { google } from '@ai-sdk/google';
+import { saveGeneratedContent } from '@/lib/db/content';
+import { ContentGenerationResponse } from '@/lib/schema';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 // gemini-2.5-flash-preview-05-20
 const model = google('gemini-2.5-flash');
 
 export async function POST(req: Request) {
-    const { prompt } = await req.json();
+    const { prompt, searchResults, contentType, tags } = await req.json();
 
-    if (!prompt) {
-        return NextResponse.json({ error: "Prompt is required" }, { status: 400 });
+    if (!prompt || !searchResults || !contentType || !tags) {
+        return NextResponse.json({ error: "Missing required parameters" }, { status: 400 });
     }
+
+    const formatedResult = searchResults.map((result, i) =>  `result [${i + 1}]: ${result.snippet} 
+ source: ${result.source}
+`);
 
     try {
       const result = streamText({
@@ -24,20 +30,30 @@ export async function POST(req: Request) {
 
 
         // Pass the user's specific request
-        prompt: `Generate a comprehensive article on the topic: "${prompt}"`,
+        prompt: `Hare is the user prompt: "${prompt}" 
+ and hare is the search results: ${formatedResult}`,
       });
 
+        let fullContent = "";
+        for await (const chunk of result.textStream) {
+            fullContent += chunk;
+        }
+
+        const contentToSave: ContentGenerationResponse = {
+            contentType: contentType,
+            contentKeyword: prompt,
+            tags: tags,
+            mainContent: fullContent,
+        };
+
+        await saveGeneratedContent(contentToSave);
+
         const stream = new ReadableStream({
-            async start(controller) {
-                for await (const chunk of result.textStream) {
-                  console.log("from api route",chunk);
-                    controller.enqueue(chunk);
-                }
+            start(controller) {
+                controller.enqueue(fullContent);
                 controller.close();
             },
         });
-
-
 
         return new Response(stream);
     } catch (error) {
